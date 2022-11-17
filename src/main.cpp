9adc658a95bca24167563a439813c0e64c6b452e
Skyggebjerg <ole@skyggebjerg.dk>
2022-11-17 12:23:00 +0100

    Two K-meter sensors

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,15 @@ M5_KMeter sensor;
 #define PWM1_Res   8
 #define PWM1_Freq  1000
 
- 
+uint8_t addr1 = 0x60;
+
 int PWM1_DutyCycle = 0;
 int DIR = 25;
- 
+
+float temperature;
+float temperature2;
+long tidsbavl = millis();
+
 void setup()
 {
   pinMode(DIR, OUTPUT);
@@ -25,34 +30,62 @@ void setup()
   ledcSetup(PWM1_Ch, PWM1_Freq, PWM1_Res);
   Wire.begin(26, 32, 400000L); //Wire.begin((int)SDA, (int)SCL, 400000L);
       // prepare sensor. ( default I2C addr : 0x66 )
-    sensor.begin();
+    //sensor.begin();
+
+        // change I2C addr.
+    // ( When the unit is turned back on, it will operate with the new I2C address. )
+  //sensor.changeAddr(0x60);
+  //sensor.begin(&Wire, 0x66);
         // set sleep time. (5 second)
     //sensor.setSleepTime(5);
     Serial.begin(115200);
 
       ledcWrite(PWM1_Ch, 255);
+     
 }
  
 void loop()
 {
+
     // Sensor deep sleep.
     // (Number of seconds set by the setSleepTime function)
     //sensor.sleep();
   
-    delay(10);
+    //delay(10);
 
     // data read from unit.
+    sensor.begin(&Wire, 0x66); // blue plot - top sensor
     if (sensor.update()) {
 
         // get sensor temperature.
-        float temperature = sensor.getTemperature();
+        temperature = sensor.getTemperature();
 
         // get unit internal temperature.
-        float internaltemp = sensor.getInternalTemp();
+        //float internaltemp = sensor.getInternalTemp();
+        //Serial.printf("%3.2f  /  %3.2f \n", temperature, temperature2);
+        //Serial.printf("%3.2f  /  %3.2f \n", temperature, internaltemp);
+       //if (temperature > 95) digitalWrite(DIR, HIGH);
+       //       if (temperature < 95) digitalWrite(DIR, LOW);
+       // if (temperature >= 80) ledcWrite(PWM1_Ch, 0);
+       // if (temperature <= 40) ledcWrite(PWM1_Ch, 255);
+    } else {
+        Serial.printf("error %d \n", sensor.getError());
+    }
+//float temp1 = temperature; 
+    sensor.begin(&Wire, 0x60); // red plot - bottom sensor
+    if (sensor.update()) {
 
-        Serial.printf("%3.2f  /  %3.2f \n", temperature, internaltemp);
-       if (temperature > 95) digitalWrite(DIR, HIGH);
-              if (temperature < 95) digitalWrite(DIR, LOW);
+        // get sensor temperature.
+        temperature2 = sensor.getTemperature();
+
+        // get unit internal temperature.
+        //float internaltemp = sensor.getInternalTemp();
+      if (millis()>=tidsbavl+250){
+        Serial.printf("%3.2f  /  %3.2f \n", temperature, temperature2);
+        tidsbavl=millis();
+    }
+       if (temperature2 > 96.5) digitalWrite(DIR, HIGH);
+              if (temperature2 < 96.5) digitalWrite(DIR, LOW);
        // if (temperature >= 80) ledcWrite(PWM1_Ch, 0);
        // if (temperature <= 40) ledcWrite(PWM1_Ch, 255);
     } else {
